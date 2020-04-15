b64bf2805c84e57ad1a60cef67d5203647d19763
Alexey Chernov <4ernov@gmail.com>
2020-04-15 21:23:08 +0300

    Try using non-TF compliant MobileNetV3 implementation
    To try being more compatible with ONNX export.

--- backbone/rw_mobilenetv3.py
+++ backbone/rw_mobilenetv3.py
@@ -5,12 +5,12 @@ import torch
 import backbone.outlet.pytorch_image_models.timm as outlet
 
 
-MobileNetV3_Large = functools.partial(outlet.models.tf_mobilenetv3_large_100,
+MobileNetV3_Large = functools.partial(outlet.models.mobilenetv3_large_100,
                                       features_only=True,
                                       out_indices=(2, 3),
                                       feature_location="bottleneck")
 
-MobileNetV3_Small = functools.partial(outlet.models.tf_mobilenetv3_small_100,
+MobileNetV3_Small = functools.partial(outlet.models.mobilenetv3_small_100,
                                       features_only=True,
                                       out_indices=(2, 3),
                                       feature_location="bottleneck")
