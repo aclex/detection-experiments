3f91c27999d45264373be530e45f596c83975c3b
Alexey Chernov <4ernov@gmail.com>
2020-05-16 01:47:30 +0300

    Fix feature extraction block numbers for Large

--- backbone/rw_mobilenetv3.py
+++ backbone/rw_mobilenetv3.py
@@ -7,7 +7,7 @@ import backbone.outlet.pytorch_image_models.timm as outlet
 
 MobileNetV3_Large = functools.partial(outlet.models.mobilenetv3_large_100,
                                       features_only=True,
-                                      out_indices=(2, 3),
+                                      out_indices=(3, 4),
                                       feature_location="bottleneck")
 
 MobileNetV3_Small = functools.partial(outlet.models.mobilenetv3_small_100,
