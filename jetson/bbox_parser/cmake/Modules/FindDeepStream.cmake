e23b98ae057e7da97352ce86828a6156f6dbc2e6
Alexey Chernov <4ernov@gmail.com>
2020-07-20 02:14:20 +0300

    Enumerate all the libraries in `FindDeepStream.cmake`
    Otherwise this module tends to be less general.

--- jetson/bbox_parser/cmake/Modules/FindDeepStream.cmake
+++ jetson/bbox_parser/cmake/Modules/FindDeepStream.cmake
@@ -25,15 +25,42 @@ include(FindPackageHandleStandardArgs)
 
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(DeepStream REQUIRED_VARS DeepStream_INCLUDE_DIR DeepStream_LIBRARY VERSION_VAR DeepStream_VERSION_STRING)
 
+set(DeepStream_LIBRARY_NAMES
+	libnvbufsurface.so
+	libnvds_infer.so
+	libnvds_msgconv.so
+	libnvbufsurftransform.so
+	libnvds_amqp_proto.so
+	libnvds_inferutils.so
+	libnvds_nvdcf.so
+	libnvds_azure_edge_proto.so
+	libnvds_kafka_proto.so
+	libnvds_nvtxhelper.so
+	libnvds_azure_proto.so
+	libnvds_logger.so
+	libnvds_csvparser.so
+	libnvds_meta.so
+	libnvds_osd.so
+	libnvdsgst_helper.so
+	libnvds_mot_iou.so
+	libnvds_utils.so
+	libnvdsgst_meta.so
+	libnvds_mot_klt.so
+	)
+
 if (DeepStream_FOUND)
 	set(DeepStream_INCLUDE_DIRS ${DeepStream_INCLUDE_DIR})
-	set(DeepStream_LIBRARIES ${DeepStream_LIBRARY})
+
+	get_filename_component(DeepStream_LIBRARY_DIR ${DeepStream_LIBRARY} DIRECTORY)
+	set(DeepStream_LIBRARIES ${DeepStream_LIBRARY_NAMES})
+	list(TRANSFORM DeepStream_LIBRARIES PREPEND "${DeepStream_LIBRARY_DIR}/")
 
 	if (NOT TARGET DeepStream::DeepStream)
-		add_library(DeepStream::DeepStream UNKNOWN IMPORTED)
+		add_library(DeepStream::DeepStream INTERFACE IMPORTED)
 		set_target_properties(DeepStream::DeepStream PROPERTIES
 			INTERFACE_INCLUDE_DIRECTORIES "${DeepStream_INCLUDE_DIRS}"
-			IMPORTED_LOCATION "${DeepStream_LIBRARIES}")
+			INTERFACE_LINK_LIBRARIES "${DeepStream_LIBRARIES}"
+			)
 	endif()
 endif()
 
