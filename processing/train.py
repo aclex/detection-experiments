d8a140e018607cb7a46134c13b568dbc7abdaf7c
Alexey Chernov <4ernov@gmail.com>
2021-06-24 22:14:25 +0300

    Partially remove deprecated augmentations
    No chance to refuse of `Cutout` yet as `CoarseDropout` seems to
    suprisingly unable to process bboxes.

--- processing/train.py
+++ processing/train.py
@@ -9,7 +9,7 @@ from processing.get_aug import GetAug
 class Pipeline(GetAug):
 	def __init__(self, size, mean, std, bbox_format='albumentations'):
 		super(Pipeline, self).__init__([
-			A.RandomContrast(),
+			A.RandomBrightnessContrast(),
 			A.RandomGamma(),
 			A.HueSaturationValue(sat_shift_limit=50, p=0.6),
 			A.CLAHE(),
